abd733a2171b594f128f0752e38b4232a64cc029
Fernando Tubio <fernando.tubio@southworks.net>
2016-01-06 23:44:23 -0300

    Simplified parameters to getIcon method in iconTools

--- src/iconTools.js
+++ src/iconTools.js
@@ -1,17 +1,15 @@
 'use strict';
 
-var url = require('url'),
-		path = require('path'),
+var path = require('path'),
+    url = require('url'),
 		Q = require('q');
 
 var fileTools = require('./fileTools'),
 		download = require('./download'),
 		log = require('./log');
 
-function getIcon(iconPath, startUrl, baseDir, callback) {
+function getIcon(iconUrl, iconFilePath, callback) {
 
-	var iconUrl = url.resolve(startUrl, iconPath);
-	var iconFilePath = path.join(baseDir, iconPath);
 	var iconFolder = path.dirname(iconFilePath);
 
 	log.debug('Downloading icon file from [' + iconUrl + '] to [' + iconFilePath + ']');
