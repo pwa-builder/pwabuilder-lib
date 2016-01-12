6f8ef428f81422a08325e20bcfdf93ec024cd656
Fernando Tubio <fernando.tubio@southworks.net>
2016-01-12 16:24:57 -0300

    Added write method to log to show messages regardless of the log level

--- src/log.js
+++ src/log.js
@@ -15,17 +15,28 @@ if (!log) {
   var originalFactory = log.methodFactory;
   log.methodFactory = function (methodName, logLevel, loggerName) {
     var rawMethod = originalFactory(methodName, logLevel, loggerName);
-
+    
     return function (message, source, severity) {
       message = message.replace(/\n/g, '\n' + Array(maxLenSeverity + maxLenSource + 6).join(' '));
-      source = source || loggerName || app.name;
-      severity = severity || methodName;
-      rawMethod(
-        '[' + severity + Array(Math.max(maxLenSeverity - severity.length + 1, 0)).join(' ') + '] '
-        + source + Array(Math.max(maxLenSource - source.length + 1, 0)).join(' ') + ': '
-        + message);
+      
+      if (methodName !== 'write') {
+        source = source || loggerName || app.name;      
+        severity = severity || methodName;
+        rawMethod(
+          '[' + severity + Array(Math.max(maxLenSeverity - severity.length + 1, 0)).join(' ') + '] '
+          + source + Array(Math.max(maxLenSource - source.length + 1, 0)).join(' ') + ': '
+          + message);        
+      }
+      else {
+        rawMethod(message);
+      } 
     };
   }
+  
+  log.write = log.methodFactory('write', log.levels.INFO, '');
+  
+  // call setLevel to apply the changes
+  log.setLevel(log.getLevel());
   
   global.manifoldjs_logger = log;
 }
