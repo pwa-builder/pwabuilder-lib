08e10410209db6b9e72e97c563b9d71a6fd6ff84
Fernando Tubio <fernando.tubio@southworks.net>
2016-01-23 14:33:16 -0300

    Invoke callback in loadValidationRules tests and removed obsolete test

--- test/manifestTools/validationRules.js
+++ test/manifestTools/validationRules.js
@@ -1,40 +1,37 @@
 'use strict';
 
-var validation = require('../../lib/manifestTools/validation');
-var path = require('path');
-var fs = require('fs');
+var fs = require('fs'),
+    path = require('path');
+
 require('should');
+
+var validation = require('../../lib/manifestTools/validation');
 
 var validationRulesPath = path.join(__dirname, '..', '..', 'lib', 'manifestTools', 'validationRules');
 
 describe('validationRules', function () {
   describe('Loaded modules', function () {
-    validation.loadValidationRules(validationRulesPath).then(function (validationRules) {
-      it('Should load all validationRules modules', function() {
-        validationRules.should.have.property('all');
-        validationRules.should.have.property('android');
-        validationRules.should.have.property('chrome');
-        validationRules.should.have.property('firefox');
-        validationRules.should.have.property('windows');
-        validationRules.should.have.property('ios');
-      });
-
-      it('Should load all modules (files) in the validationRules folder', function(done) {
+    it('Should load all modules (files) in the validationRules folder', function(done) {
+      validation.loadValidationRules(validationRulesPath).then(function (validationRules) {
         fs.readdir(validationRulesPath, function (err, files) {
-          var validationRulesLoadedLength = Object.keys(validationRules).length;
+          var validationRulesLoadedLength = validationRules.length;
           validationRulesLoadedLength.should.be.above(0);
           validationRulesLoadedLength.should.be.equal(files.length);
 
           done();
         });
       });
-
-      it('All loaded modules should have the same interface', function() {
-        for (var validationRules in validationRules) {
-          /*jshint -W030 */
-          validationRules[validationRules].should.be.a.Function;
-        }
-      });      
     });
+
+    it('All loaded modules should have the same interface', function(done) {
+      validation.loadValidationRules(validationRulesPath).then(function (validationRules) {
+        for (var validationRule in validationRules) {
+          /*jshint -W030 */
+          validationRules[validationRule].should.be.a.Function;
+        }
+        
+        done();
+      });
+    });      
   });
 });
