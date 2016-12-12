3a89519cb266a1a3655a5c5190e878b2ac8bb08e
Ezequiel Jadib <ezequiel.jadib@southworks.net>
2016-12-12 09:38:30 -0500

    Add Service worker tools operations

---
+++ assets/serviceWorkers/serviceWorker2/manifoldjs-sw.js
@@ -0,0 +1,8 @@
+//This is Offline copy of pages Service Worker
+self.addEventListener('install', function(event) {
+	//TODO: Add code here
+});
+
+self.addEventListener('fetch', function(event) {
+	//TODO: Add code here
+});
---
+++ assets/serviceWorkers/serviceWorker3/manifoldjs-sw.js
@@ -0,0 +1,8 @@
+//This is combined Offline Experience (Offline page + Offline copy of pages) Service Worker
+self.addEventListener('install', function(event) {
+	//TODO: Add code here
+});
+
+self.addEventListener('fetch', function(event) {
+	//TODO: Add code here
+});
